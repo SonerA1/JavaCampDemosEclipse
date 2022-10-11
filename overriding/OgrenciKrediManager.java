2c427a5c2650ce9361e1c6c97c3085d9d006c5d9
Soner <114054564+SonerA1@users.noreply.github.com>
2022-10-11 10:56:21 +0300

    Delete overriding directory

--- overriding/OgrenciKrediManager.java
+++
@@ -1,8 +0,0 @@
-package overriding;
-
-public class OgrenciKrediManager extends BaseKrediManager {
-	public double hesapla(double tutar) { //  base kredimanagera public final double hesapla() şeklinde de yazılabilirdi
-		return tutar * 1.10; // override ederek ogrenci kredisini 1.10 olarak aldık
-		// base kredi manager bu durumu kabul ederek devam eder.
-	}
-}
