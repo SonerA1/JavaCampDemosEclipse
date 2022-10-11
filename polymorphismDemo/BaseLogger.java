0cbdea67b8b8c3e50ab9b0fedf85181c925ed9f0
Soner <114054564+SonerA1@users.noreply.github.com>
2022-10-11 10:56:29 +0300

    Delete polymorphismDemo directory

--- polymorphismDemo/BaseLogger.java
+++
@@ -1,9 +0,0 @@
-package polymorphismDemo;
-
-public class BaseLogger {
-	public void log(String message) {
-		System.out.println("Default logger: " + message);
-
-	}
-
-}
--- polymorphismDemo/FileLogger.java
+++
@@ -1,7 +0,0 @@
-package polymorphismDemo;
-
-public class FileLogger extends BaseLogger {
-	public void log(String message) {
-		System.out.println("Logged to file" + message);
-	}
-}
