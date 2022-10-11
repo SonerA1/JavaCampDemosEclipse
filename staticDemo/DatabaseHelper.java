e7f3141178da73d5d430a2012cf850807211cad4
Soner <114054564+SonerA1@users.noreply.github.com>
2022-10-11 10:54:47 +0300

    Day3

---
+++ staticDemo/DatabaseHelper.java
@@ -0,0 +1,23 @@
+package staticDemo;
+
+public class DatabaseHelper {
+	public static class Crud {// create read update delete
+		public static void Delete() {
+
+		}
+
+		public static void Update() {
+
+		}
+
+	}
+
+	public static class Connection {
+		public static void createConnection() {
+
+		}
+	}
+
+	// bu tip codeları önerilmiyor.
+
+}
